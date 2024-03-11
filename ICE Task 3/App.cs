f3e05e1fb520939c5214ed7f3594de120f80d17c
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2024-03-11 13:48:24 +0200

    Link local and remote repository

---
+++ ICE Task 3/App.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (C) 2024 ST10257002
+// This is an integrated curriculum engagement assignment task submission for
+// Programming 2A (VCGPPT-PROG6221-FT-BCAD0701)
+
+internal class App {
+
+    private static void Main(string[] args) {
+    }
+}
